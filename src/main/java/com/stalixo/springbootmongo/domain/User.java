f0ff4af9478d18801f0933fe61bd792187af9004
Lucas <lucasgabrielsantos398@gmail.com>
2024-06-29 11:47:08 -0300

    "Conectando ao MongoDB com repository e service"

--- src/main/java/com/stalixo/springbootmongo/domain/User.java
+++ src/main/java/com/stalixo/springbootmongo/domain/User.java
@@ -1,11 +1,16 @@
 package com.stalixo.springbootmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document(collection = "user")
 public class User implements Serializable {
     private static long serialVersionUID = 1L;
 
+    @Id
     private String id;
     private String name;
     private String email;
