f54f4d365c409f96a82b299ca861515b05a82040
Lucas Gabriel <lucasgabrielsantos398@gmail.com>
2024-07-03 14:55:23 -0300

    "Obtendo post por id"

---
+++ src/main/java/com/stalixo/springbootmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.stalixo.springbootmongo.services;
+
+import com.stalixo.springbootmongo.domain.Post;
+import com.stalixo.springbootmongo.repository.PostRepository;
+import com.stalixo.springbootmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public List<Post> findAll() {
+        return repo.findAll();
+    }
+
+    public Post findById(String id) {
+        Optional<Post> user = repo.findById(id);
+        user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        return user.get();
+    }
+
+}
