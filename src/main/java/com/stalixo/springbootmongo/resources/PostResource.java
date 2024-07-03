8dca37375c59a7ac3eaa78994fb56368406fddd0
Lucas Gabriel <lucasgabrielsantos398@gmail.com>
2024-07-03 15:29:06 -0300

    "Consulta simples com Query Methods"

--- src/main/java/com/stalixo/springbootmongo/resources/PostResource.java
+++ src/main/java/com/stalixo/springbootmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.stalixo.springbootmongo.resources;
 
 import com.stalixo.springbootmongo.domain.Post;
+import com.stalixo.springbootmongo.resources.util.URL;
 import com.stalixo.springbootmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -19,6 +22,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
